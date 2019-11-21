98e0927260e473a376e2d45f40e75d672bcbd02a
jacobcmalimban <jacobcmalimban@gmail.com>
2019-11-21 21:08:18 +0000

    Move Files

---
+++ compiler/ParserProject/Token.java
@@ -0,0 +1,9 @@
+
+/**
+ *  Jacob Malimban
+ * Tokens to be used
+ * by both Lex and Parser
+ */
+public enum Token {
+ INT_LIT, IDENT, ASSIGN_OP, ADD_OP, SUB_OP, MULT_OP, DIV_OP, LEFT_PAREN, RIGHT_PAREN, END_OF_LINE, END_OF_FILE
+}
