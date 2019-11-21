68e647ddad71df97b655354fd266f82ab64ad9c1
jacobcmalimban <jacobcmalimban@gmail.com>
2019-11-21 21:06:35 +0000

    Cleanup

--- java/ParserProject/Token.java
+++
@@ -1,9 +0,0 @@
-
-/**
- *  Jacob Malimban
- * Tokens to be used
- * by both Lex and Parser
- */
-public enum Token {
- INT_LIT, IDENT, ASSIGN_OP, ADD_OP, SUB_OP, MULT_OP, DIV_OP, LEFT_PAREN, RIGHT_PAREN, END_OF_LINE, END_OF_FILE
-}
--- java/ParserProject/bak/Token.java
+++
@@ -1,8 +0,0 @@
-/**
- *  Jacob Malimban
- *
- *
- */
-public enum Token {
-	INT_LIT, IDENT, ASSIGN_OP, ADD_OP, SUB_OP, MULT_OP, DIV_OP, LEFT_PAREN, RIGHT_PAREN, END_OF_LINE, END_OF_FILE
-}
