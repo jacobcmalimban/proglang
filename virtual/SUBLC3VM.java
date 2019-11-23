d8897caa8305d7bb67a556cd181868fecf168b23
jacobcmalimban <jacobcmalimban@gmail.com>
2019-11-23 12:46:00 +0000

    Add ideas

--- virtual/SUBLC3VM.java
+++ virtual/SUBLC3VM.java
@@ -1,5 +1,14 @@
 public class SUBLC3VM {
-	public static void main(String[] args){
+	private static 
+	private static
+	public static void main(String[] args) {
 		System.out.println("Hello virtual");
+	}
+	private static void OUT(String out) {
+	System.out.println(out);
+	}
+	private static void STO (String var, String data) {
+	}
+	private static
 	}
 }
