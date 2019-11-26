65fce70034142b33a99f6007a02bc718b927f038
jacobcmalimban <jacobcmalimban@gmail.com>
2019-11-26 23:08:43 +0000

    Heartache with Functions

--- haskell/First/halloWelt.hs
+++ haskell/First/halloWelt.hs
@@ -1,4 +1,13 @@
+--testFun :: String -- input
+--        -> String -- output
+--testFun t = sort t
+
 main = do
-    putStrLn "konnichiha, waruldo\nWie heisst du?\n> "
-    name <- getLine
-    putStrLn ("Hoi " ++ name ++ ", shiawase ni narimasuyouni")
+    putStr "konnichiha, waruldo\nWie heisst du?\n> "
+    name <- getLine    
+    putStrLn ( "Hoi " ++ name ++ ", shiawase ni narimasuyouni")
+
+--    let print = putStrLn
+--    print("Was ist dein Quest?\n> ")
+--    fun <- testFun getLine
+--    putStrLn $ "I c, you are going to: " ++ fun
