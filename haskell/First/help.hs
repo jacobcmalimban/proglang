699724926d7812c6177ee2b4f2510988eb488db4
jacobcmalimban <jacobcmalimban@gmail.com>
2019-11-29 20:27:21 +0000

    re :: String -> String
    re[] = "Cursion"
    re (x:xs) = "Cursion" ++ re xs

--- haskell/First/help.hs
+++ haskell/First/help.hs
@@ -126,7 +126,6 @@ daNaem = ["Alice", "Bob", "Charlie"]
 naughtMemAddr = ["1123 West", "4565 East", "7890 Central"]
 daAddr = zip daNaem naughtMemAddr
 
-
 {- Functions
     Definition
         funcName :: (params) -> output
@@ -142,7 +141,77 @@ snd3 (_, x, _) = x
 addn :: Int -> Int -> Int
 addn a b = a + b
 
+addGenrc a b = a + b
+
+add2ple :: (Int, Int) -> (Int, Int) -> (Int, Int)
+add2ple (x, y)(x2,y2) = (x+x2, y+y2)
+
+ageChk :: Int -> String
+ageChk 21 = "No Drinking and Driving"
+ageChk 18 = "Better vote!"
+ageChk 16 = "Drive to HS?"
+ageChk x = "No drugs ~"
+
+--  recursive recursion <3
+fctrl :: Int -> Int
+fctrl 0 = 1
+fctrl n = n * fctrl (n-1)
+
+prdTo n = product [1..n]
+
+--  if statement
+oddish :: Int -> Bool
+oddish n
+    | mod n 2 == 0  = False
+    | otherwise = True
+
+elven n = (n `mod` 2 == 0)
+
+schlGrd :: Int -> String
+schlGrd age
+    | (age < 4) || (age > 30) = "Always a student"
+    | (age > 21) = "Post undergrad?"
+    | (age > 17) = "Post secondary"
+    | (age > 14) = "Secondary"
+    | otherwise = "Primary"
+
+batAvg :: Double -> Double -> String
+
+batAvg hits atBat
+    | avg <= 0.2     = "Negative standard dev"
+    | avg <= 0.25    = "Deviation = 0"
+    | avg <= 0.28    = "Deviation positive"
+    | otherwise     = "Don't get cocky."
+    where avg = hits/atBat
+
+lsTems :: [Int] -> String
+lsTems []       = "List empty"
+lsTems (x:[])   = "List contains only: " ++ show x
+-- 2 or more
+lsTems (x:y:[]) = "List starts with: " ++ show x ++ ", then only " ++ show y
+lsTems (x:rest) = "1st item is: " ++ show x ++ ", followed by: " ++ show rest
+
+fursTem :: String -> String
+fursTem []          = "String is emtpy"
+fursTem all@(x:xs)  = "First letter is " ++ [x] ++ " from " ++ all
+
+-- map (*4) [1..10]
+mul4 :: Int -> Int
+mul4 x = x*4
+
+lsT4 = map mul4 [1..10]
+
+multBy4 :: [Int] -> [Int]       -- pffh recursion
+multBy4 [] = []
+multBy4 (x:xs) = mul4 x : multBy4 xs
+
+strEql :: [Char] -> [Char] -> Bool
+strEql [] []            = True
+strEql (x:xs) (y:ys)    = x == y && strEql xs ys
+strEql _ _              = False
+
 main = do
     putStrLn "Who are you?"
     youwu <- getLine
     putStrLn ("Hello " ++ youwu)
+
