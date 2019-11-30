0c16ca3a8c23b4412d6be025b9e5f688c3e8a863
jacobcmalimban <jacobcmalimban@gmail.com>
2019-11-30 02:05:52 +0000

    Input Strings to Ints

--- haskell/NumLtAvg.hs
+++ haskell/NumLtAvg.hs
@@ -1,13 +1,27 @@
 
-purifiedNums :: Int -> [String] -> [Int]
+purifiedNums :: Int -> [Int] -> [Int]
 purifiedNums 0 _ = []
-purifiedNums counter list = 
-    [ read (head list) :: Int
-        : purifiedNums (counter + (-1)) tail list ]
+purifiedNums counter list =  
+    head list : purifiedNums (counter - 1) (tail list)
 
+strToInt :: [String] -> [Int]
+strToInt strLst = map (\x -> read x :: Int) strLst
 
 main = do
     putStrLn "Enter the count and the corresponding integer values:"
     line <- getLine
+
+{-
+    putStrLn(
+        purifiedNums
+        (head (strToInt(words line))
+        (tail (strToInt(words line))
+    )
+-}
+
+
+--    putStrLn purifiedNums ( head ( map (\x -> read x :: Int) (words line)) ) ( tail ( map (\x -> read x :: Int) (words line)) )
+
+--  ints = words line
 --  map (\x -> read x :: Int)    
-    putStrLn "The list:"
+    putStrLn("The list: " ++ line)
