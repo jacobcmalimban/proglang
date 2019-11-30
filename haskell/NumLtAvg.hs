ca6448eebe24d1e1db8ea4342bee3982ed89ea1b
jacobcmalimban <jacobcmalimban@gmail.com>
2019-11-30 03:44:34 +0000

    Count Averages

--- haskell/NumLtAvg.hs
+++ haskell/NumLtAvg.hs
@@ -1,3 +1,7 @@
+import Data.List
+
+show' :: Show a => [a] -> String
+show' = intercalate " " . map show
 
 purifiedNums :: Int -> [Int] -> [Int]
 purifiedNums 0 _ = []
@@ -7,21 +11,46 @@ purifiedNums counter list =
 strToInt :: [String] -> [Int]
 strToInt strLst = map (\x -> read x :: Int) strLst
 
+--fmt :: String -> ([String] -> [Int])
+fmt line = strToInt(words line)
+
+
 main = do
-    putStrLn "Enter the count and the corresponding integer values:"
+    putStrLn "Enter the count and the corresponding integer values: "
     line <- getLine
 
+    let listlen = fromIntegral (head (fmt line))
+    let purified = purifiedNums (head (fmt line)) (tail (fmt line)) 
+
+    putStrLn("The list: ")
+    putStrLn $ id(show' purified)
+
+    putStrLn("The average: ")
+    let inSum = fromIntegral (sum purified)
+    let avg = inSum / listlen
+    putStrLn $ show( avg )
+
+    putStrLn("Number of values less than average: ")
+    let ltAvg = filter (< avg) (map (fromIntegral) purified)
+
+
+    putStrLn $ show(length ltAvg)
+--    putStrLn( length ltAvg )
+
+
+--(fromIntegral (sum purified)) / (length purified) )
+-- sum purified  /   
+--    length purified )
+
+
+--    putStrLn $ show ( (sum(map (fromIntegral) purified)) / length purified)
+
+--    putStrLn(     putStrLn $ show ( sum purified / length purified )
+
+
 {-
-    putStrLn(
-        purifiedNums
-        (head (strToInt(words line))
-        (tail (strToInt(words line))
-    )
+
+    putStrLn("Number of values less than average:")
+    putStrLn()
+
 -}
-
-
---    putStrLn purifiedNums ( head ( map (\x -> read x :: Int) (words line)) ) ( tail ( map (\x -> read x :: Int) (words line)) )
-
---  ints = words line
---  map (\x -> read x :: Int)    
-    putStrLn("The list: " ++ line)
